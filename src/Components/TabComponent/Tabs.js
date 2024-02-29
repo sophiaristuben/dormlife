de453a76735cc59c4d5e073b05c4a81d3b94e166
sophiaristuben <sophiaristuben@gmail.com>
2024-02-28 21:12:18 -0800

    set up for 2/29

--- src/Components/TabComponent/Tabs.js
+++ src/Components/TabComponent/Tabs.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import TabNavItem from "./TabNavItem.js";
 import TabContent from "./TabContent.js";
 
-// Import our Map Components
+// import map components
 import MapboxComponent from "../MapComponents/MapboxComponent.js";
+
+// import your 
+import SophiaComponent from "../AboutMeComponents/SophiaComponent/SophiaComponent.js";
+
 
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState("mapbox");
@@ -11,13 +15,45 @@ const Tabs = () => {
   return (
     <div className="Tabs">
       <ul className="nav">
-        <TabNavItem title="Mapbox" id="mapbox" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Map" id="mapbox" activeTab={activeTab} setActiveTab={setActiveTab}/>
+
+        <TabNavItem title="Sophia" id="sophia" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Rachel" id="rachel" activeTab={activeTab} setActiveTab={setActiveTab}/>
+
+        <TabNavItem title="Adrien" id="adrien" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Max" id="max" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Diya" id="diya" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Catherine" id="catherine" activeTab={activeTab} setActiveTab={setActiveTab}/>
+        <TabNavItem title="Georgia" id="georgia" activeTab={activeTab} setActiveTab={setActiveTab}/>
       </ul>
  
       <div className="outlet">
         <TabContent id="mapbox" activeTab={activeTab}>
-        <MapboxComponent></MapboxComponent>
+          <MapboxComponent></MapboxComponent>
         </TabContent>
+
+        <TabContent id="sophia" activeTab={activeTab}>
+          <SophiaComponent></SophiaComponent>
+        </TabContent>
+        <TabContent id="rachel" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+        <TabContent id="adrien" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+        <TabContent id="max" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+        <TabContent id="diya" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+        <TabContent id="catherine" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+        <TabContent id="georgia" activeTab={activeTab}>
+          <p>Insert text...</p>
+        </TabContent>
+
       </div>
     </div>
   );
