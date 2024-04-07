b5b892c31865008affcb56d94474badb6b6914e7
cliu26 <cliu26@cmc.edu>
2024-04-07 13:27:18 -0700

    buttons

--- src/Components/TabComponent/Tabs.js
+++ src/Components/TabComponent/Tabs.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { DormNav } from "../DormNav/DormNav";
+import ThreeFirstButton from './3_1_FloorButton.png';
+import ThreeSecondButton from './3_2_FloorButton.png';
+import ThreeThirdButton from './3_3_FloorButton.png';
 
 const Tabs = ({ currSchool, activeTab, setActiveTab }) => {
   return (
