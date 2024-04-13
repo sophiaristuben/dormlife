819b65123e98917eff4d129a18eb8c7e241c48b9
mvpodell <mvpa2020@mymail.pomona.edu>
2024-04-13 14:30:21 -0700

    styling home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,15 +6,21 @@ export const Home = () => {
 
     return (
         <div className="homeContainer">
-            <h1>Homepage</h1>
-            <nav className="homeHeader">
-                <Link className="school pomona" to={"/pomona"} >Pomona </Link>
-                <Link className="school scripps" to={"/scripps"}>Scripps </Link>
-                <Link className="school cmc" to={"/cmc"}>CMC </Link>
-                <Link className="school pitzer" to={"/pitzer"}>Pitzer </Link>
-                <Link className="school mudd" to={"/mudd"}>Mudd </Link>
-            </nav>
-            {/* <Outlet context={[currSchool, setCurrSchool]}/> */}
+            <div className="homeHeader">
+            <h1>p-dormlife</h1>
+            </div>
+            <div className="homeBody">
+                <nav className="homeNav">
+                    <Link className="school pomona" to={"/pomona"} >Pomona </Link>
+                    <Link className="school scripps" to={"/scripps"}>Scripps </Link>
+                    <Link className="school cmc" to={"/cmc"}>CMC </Link>
+                    <Link className="school pitzer" to={"/pitzer"}>Pitzer </Link>
+                    <Link className="school mudd" to={"/mudd"}>Mudd </Link>
+                </nav>
+                <div className="homePanel">
+                    <p>Backend team can put stuff on the home page here!</p>
+                </div>
+            </div>
         </div>
     );
 }
