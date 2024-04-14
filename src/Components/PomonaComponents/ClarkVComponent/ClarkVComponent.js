da6538942a43a538d4afdcacf8c1cd0fd3289fff
cliu26 <cliu26@cmc.edu>
2024-04-14 13:06:33 -0700

    buttons and maps

--- src/Components/PomonaComponents/ClarkVComponent/ClarkVComponent.js
+++ src/Components/PomonaComponents/ClarkVComponent/ClarkVComponent.js
@@ -1,21 +1,49 @@
+import React, { useState } from "react";
 import TabContent from "../../TabComponents/TabContent";
 import { DormMap } from "../../DormNav/DormMap/DormMap";
 import { Popup } from "../../DormNav/Popup/Popup";
-import TahoeHouse from "../SontagComponent/TahoeHouse2005.jpg";
+import ClarkVFloor1 from "/Users/catherineliu/dormlife/src/Components/DormNav/DormMap/Maps/ClarkV/ClarkV1.png";
+import ClarkVFloor2 from "/Users/catherineliu/dormlife/src/Components/DormNav/DormMap/Maps/ClarkV/ClarkV2.png";
+import FirstFloorButton from "../Buttons/Two Floors/2_1_FloorButton.png";
+import SecondFloorButton from "../Buttons/Two Floors/2_2_FloorButton.png";
 
-const ClarkVComponent = ({activeTab, clickedArea, setClickedArea, showPopup, setShowPopup}) => {
+const ClarkVComponent = ({ activeTab, clickedArea, setClickedArea, showPopup, setShowPopup }) => {
+    const [activeMap, setActiveMap] = useState(ClarkVFloor1);
+
+    const switchToFirstFloor = () => {
+        setActiveMap(ClarkVFloor1);
+    };
+
+    const switchToSecondFloor = () => {
+        setActiveMap(ClarkVFloor2);
+    };
+
     return (
         <>
             <TabContent id={activeTab} activeTab={activeTab}>
-                <div className="dormMapContainer">
-                    <DormMap setShowPopup={setShowPopup} setClickedArea={setClickedArea} map={TahoeHouse} activeTab={activeTab}/>
+                <div className="dormMapContainer" style={{ position: 'relative', overflow: 'hidden', width: '100%', height: '100vh', padding: '20px' }}>
+                    <DormMap setShowPopup={setShowPopup} setClickedArea={setClickedArea} map={activeMap} activeTab={activeTab} />
                     {showPopup && (
                         <Popup {...clickedArea} />
                     )}
+                    {!showPopup && (
+                        <div className="dormInfo">
+                            <h3>ClarkV!</h3>
+                            <p> ... </p>
+                        </div>
+                    )}
                 </div>
             </TabContent>
+            <div className="sidePanel" style={{ position: 'absolute', top: '50%', left: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', zIndex: '999' }}>
+                 <button style={{ width: '50px', height: '80px', margin: '0px', padding: '0', border: 'none', background: 'none', cursor: 'pointer' }} onClick={switchToFirstFloor}>
+                    <img src={FirstFloorButton} alt="First Floor" style={{ width: '100%', height: '100%' }} />
+                </button>
+                <button style={{ width: '50px', height: '80px', margin: '35px', padding: '0', border: 'none', background: 'none', cursor: 'pointer' }} onClick={switchToSecondFloor}>
+                    <img src={SecondFloorButton} alt="Second Floor" style={{ width: '100%', height: '100%' }} />
+                </button>
+            </div>
         </>
-    )
+    );
 };
 
 export default ClarkVComponent;
--- src/Components/PomonaComponents/WalkerComponent/WalkerComponent.js
+++ src/Components/PomonaComponents/WalkerComponent/WalkerComponent.js
@@ -1,16 +1,28 @@
-import "./WalkerComponent.css";
-
+import React, { useState } from "react";
 import TabContent from "../../TabComponents/TabContent";
 import { DormMap } from "../../DormNav/DormMap/DormMap";
 import { Popup } from "../../DormNav/Popup/Popup";
 import WalkerFloor1 from "../../DormNav/DormMap/WalkerFloor1.png";
+import WalkerFloor2 from "../../DormNav/DormMap/WalkerMap2.png";
+import FirstFloorButton from "../Buttons/Two Floors/2_1_FloorButton.png";
+import SecondFloorButton from "../Buttons/Two Floors/2_2_FloorButton.png";
 
-const WalkerComponent = ({activeTab, clickedArea, setClickedArea, showPopup, setShowPopup}) => {
+const WalkerComponent = ({ activeTab, clickedArea, setClickedArea, showPopup, setShowPopup }) => {
+    const [activeMap, setActiveMap] = useState(WalkerFloor1);
+
+    const switchToFirstFloor = () => {
+        setActiveMap(WalkerFloor1);
+    };
+
+    const switchToSecondFloor = () => {
+        setActiveMap(WalkerFloor2);
+    };
+
     return (
         <>
             <TabContent id={activeTab} activeTab={activeTab}>
-                <div className="dormMapContainer">
-                    <DormMap setShowPopup={setShowPopup} setClickedArea={setClickedArea} map={WalkerFloor1} activeTab={activeTab}/>
+                <div className="dormMapContainer" style={{ position: 'relative', overflow: 'hidden', width: '100%', height: '100vh', padding: '20px' }}>
+                    <DormMap setShowPopup={setShowPopup} setClickedArea={setClickedArea} map={activeMap} activeTab={activeTab} />
                     {showPopup && (
                         <Popup {...clickedArea} />
                     )}
@@ -22,8 +34,16 @@ const WalkerComponent = ({activeTab, clickedArea, setClickedArea, showPopup, set
                     )}
                 </div>
             </TabContent>
+            <div className="sidePanel" style={{ position: 'absolute', top: '50%', left: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', zIndex: '999' }}>
+                 <button style={{ width: '50px', height: '80px', margin: '0px', padding: '0', border: 'none', background: 'none', cursor: 'pointer' }} onClick={switchToFirstFloor}>
+                    <img src={FirstFloorButton} alt="First Floor" style={{ width: '100%', height: '100%' }} />
+                </button>
+                <button style={{ width: '50px', height: '80px', margin: '35px', padding: '0', border: 'none', background: 'none', cursor: 'pointer' }} onClick={switchToSecondFloor}>
+                    <img src={SecondFloorButton} alt="Second Floor" style={{ width: '100%', height: '100%' }} />
+                </button>
+            </div>
         </>
-    )
+    );
 };
 
 export default WalkerComponent;
